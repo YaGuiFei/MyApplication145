22ca94ba2eb5fc961ebd1fd9354732de1e168056
MGR <913982037@qq.com>
2020-03-11 21:56:50 +0800

    C6

--- app/src/main/java/com/example/myapplication145/MainActivity.java
+++ app/src/main/java/com/example/myapplication145/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("四号");
     }
 }
